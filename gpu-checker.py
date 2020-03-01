a1e8676955ac35415215f85aeea6b32faed2e0e0
zmichek <zmichekk@gmail.com>
2020-03-01 08:12:19 -0600

    Remove jupyter notebook gpu checker

--- gpu-checker.py
+++ gpu-checker.py
@@ -1,6 +1,12 @@
 import torch
 def gpu_checker():
-    return (torch.cuda.current_device(), torch.cuda.device(0),
-    torch.cuda.device_count(), torch.cuda.get_device_name(0),
+    return (torch.cuda.device_count(), torch.cuda.current_device(),
+    torch.cuda.device(0), torch.cuda.get_device_name(0),
     torch.cuda.is_available())
-print(gpu_checker())
+
+
+print('Devices Connected: ', gpu_checker()[0])
+print('Device In Use: ', gpu_checker()[1])
+print('Device Location: ', gpu_checker()[2])
+print('Device Name: ', gpu_checker()[3])
+print('Is Device Available: ', gpu_checker()[4])
